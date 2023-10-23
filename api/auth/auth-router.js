87f526b191063fa16faa1a1da06ebf2b0b0b223d
usnar <usna.rafiqzadah@gmail.com>
2023-10-23 01:34:12 -0400

    mvp completed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,14 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
-
-/**
+const router = require('express').Router()
+const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
+const {
+    checkUsernameFree,
+    checkPasswordLength,
+    checkUsernameExists
+} = require('./auth-middleware')
+  /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
   response:
@@ -25,6 +31,16 @@
   }
  */
 
+  router.post('/register', checkPasswordLength, checkUsernameFree,  (req, res, next) => {
+      const { username, password } = req.body
+      const hash = bcrypt.hashSync(password, 8)
+      User.add({ username, password: hash })
+      .then(saved => {
+        res.status(201).json(saved)
+      })
+      .catch(next)
+  })
+
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,6 +57,19 @@
     "message": "Invalid credentials"
   }
  */
+
+  router.post('/login', checkUsernameExists, (req, res, next) => {
+    const { password } = req.body
+    if (bcrypt.compareSync(password, req.user.password)) {
+      req.session.user = req.user
+      res.json({ message: `Welcome ${req.user.username}`})
+    } else {
+      next({
+        status: 401, 
+        message: "Invalid credentials"
+      })
+    }
+})
 
 
 /**
@@ -59,5 +88,23 @@
   }
  */
 
+  router.get('/logout', (req, res, next) => {
+    if (req.session.user) {
+      req.session.destroy(err => {
+        if (err) {
+          next(err)
+        } else {
+          res.json({
+            message: "logged out"
+          })
+        }
+      })
+    } else {
+      res.json({message:"no session" })
+    }
+})
+
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router
